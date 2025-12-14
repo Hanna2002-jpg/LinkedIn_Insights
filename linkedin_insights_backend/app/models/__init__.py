1883b7c04f4a22e3af817b50ecc31b82b01c3426
Hanna Ansar <hannaansar024@gmail.com>
2025-12-14 13:15:23 +0530

    Remove all files for clean re-add

--- linkedin_insights_backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Models module
-from app.models.page import Page
-from app.models.post import Post
-from app.models.comment import Comment
-from app.models.employee import Employee
-
-__all__ = ["Page", "Post", "Comment", "Employee"]
--- linkedin_insights_backend/app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# Services module
-from app.services.linkedin_service import LinkedInService
-from app.services.storage_service import StorageService
-from app.services.ai_service import AIService
-
-__all__ = ["LinkedInService", "StorageService", "AIService"]
